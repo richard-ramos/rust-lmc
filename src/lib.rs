2b6900fff6d3e0046c519ba91ca1f76c2d4ba50f
Richard Ramos <info@richardramos.me>
2020-04-08 14:40:50 -0400

    format

--- src/lib.rs
+++ src/lib.rs
@@ -3,61 +3,48 @@ use text_io::read;
 use std::io;
 use std::io::Write;
 
-pub fn compute(mut mailbox: [i32; 100]) -> u32 {
-    let mut execute = true;
+pub fn compute(mut mailbox: [i32; 100]) {
     let mut program_cnt: usize = 0;
     let mut accumulator: i32 = 0;
 
-    while execute {
+    loop {
         let instruction = mailbox[program_cnt];
         let index : usize = usize::try_from(instruction % 100).unwrap();
         let code : i32 = instruction / 100;
-
         match code {
-            1 => accumulator += mailbox[index],    // ADD
-            2 => accumulator -= mailbox[index],    // SUBSTRACT
-            3 => mailbox[index] = accumulator,     // STORE
-            5 => accumulator = mailbox[index],     // LOAD
-            6 => {                                 // BRANCH
+            1 => accumulator += mailbox[index], // ADD
+            2 => accumulator -= mailbox[index], // SUBSTRACT
+            3 => mailbox[index] = accumulator, // STORE
+            5 => accumulator = mailbox[index], // LOAD
+            6 => { // BRANCH
                 program_cnt = index;
                 continue;
             },
-            7 => {                                 // BRANCH IF ZERO
+            7 => { // BRANCH IF ZERO
                 if accumulator == 0 {
                     program_cnt = index;
                     continue;
                 }
             },
-            8 => {                                 // BRANCH IF POSITIVE
+            8 => { // BRANCH IF POSITIVE
                 if accumulator >= 0 {
                     program_cnt = index;
                     continue;
                 }
             },
-            9 => {
-                if index == 1 {                    // INPUT
-                    print!("Enter a number: ");
-                    io::stdout().flush().unwrap();
-                    let value: String = read!("{}\n");
-                    accumulator = value.parse().expect("Invalid input value"); // TODO: retry
-                } else if index == 2 {             // OUTPUT
-                    println!("Output: {}", accumulator);
-                } else {
-                    panic!(format!("Unknown instruction: {}", mailbox[program_cnt]));
-                }
-            },
-            0 => {                                 // HALT
-                    execute = false;
-                    continue;
-
-            }
-            _ => {
-                panic!(format!("Unknown instruction: {}", mailbox[program_cnt]));
-                // TODO sigint
-            }
+            9 => match index {
+                    1 => { // INPUT
+                        print!("Input: ");
+                        io::stdout().flush().unwrap();
+                        let value: String = read!("{}\n");
+                        accumulator = value.parse().expect("Invalid input value");
+                    },
+                    2 => println!("Output: {}", accumulator), // OUTPUT
+                    _ => panic!(format!("Unknown instruction: {}", mailbox[program_cnt]))
+                },
+            0 => return, // HALT
+            _ => panic!(format!("Unknown instruction: {}", mailbox[program_cnt]))
         }
         program_cnt += 1;
     }
-
-    return 0;
 }
--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,11 @@ extern crate clap;
 use clap::{Arg, App};
 use std::io::{self, BufRead, BufReader};
 use std::fs::File;
-
 use lmc;
-
 
 fn main() {
     let matches = App::new("Little man computer")
         .version("1.0")
-        .author("Richard <info@richardramos.me>")
         .arg(Arg::with_name("file")
                 .short("f")
                 .long("file")
@@ -20,8 +17,8 @@ fn main() {
     
     // If --file is present, read the values from a file, otherwise, use stdin
     let reader: Box<dyn BufRead> = match matches.value_of("file") {
-        Some(_) => {
-            let path = std::path::PathBuf::from(matches.value_of("file").unwrap());
+        Some(file_path) => {
+            let path = std::path::PathBuf::from(file_path);
             let f = File::open(path).expect("File not found");
             Box::new(BufReader::new(f))
         },
@@ -32,17 +29,14 @@ fn main() {
     let mut mailbox: [i32; 100] = [0; 100];
     for line in reader.lines().enumerate() {
         let value = line.1.unwrap();
-
-        if line.0 > 99 || value == "" {
-            break; // TODO: show warning
-        }
-
         mailbox[line.0] = value.parse().expect(&format!("Invalid value on line {}", line.0 + 1));
+
+        if line.0 == 99 {
+            break;
+        }
     }
 
-    let result = lmc::compute(mailbox);
-    
-    // TODO: print result
-
+    // Execute program
+    lmc::compute(mailbox);
 }
 
